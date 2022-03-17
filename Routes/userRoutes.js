c076ae3c87a1bb396067794fa5cd93763ec7d322
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-03-18 02:01:48 +0530

    ..

--- Routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
-const router = express.Router();
-
-// Basic Login Signup Routes ----- /api/users/
-router.route('/').post(registerUser)
-router.route('/login').post(loginUser)
-
-module.exports = router;
