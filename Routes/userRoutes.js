4dcc27f4b0b35d99cec5fccccad2f70179b1a428
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-03-17 13:13:24 +0530

    name change

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,6 +3,7 @@ const { registerUser, loginUser } = require('../controllers/userController');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 
+// Basic Login Signup Routes ----- /api/users/
 router.route('/').post(registerUser)
 router.route('/login').post(loginUser)
 
