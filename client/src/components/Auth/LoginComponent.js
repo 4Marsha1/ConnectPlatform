f1c3cd7ae0de2267596dea3d6849d47774041c86
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-03-18 17:07:09 +0530

    register login state

--- client/src/components/Auth/LoginComponent.js
+++ client/src/components/Auth/LoginComponent.js
@@ -1,15 +1,46 @@
 import styles from './Auth.module.css'
 import { Link } from 'react-router-dom'
 import { ReactComponent as UserSVG } from '../../icons/user.svg'
+import { useState } from 'react'
 
 const LoginComponent = () => {
+    const [user, setUser] = useState({
+        email: '', password: '',
+    })
+    const { email, password } = user
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUser({ ...user, [name]: value })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(user);
+    }
     return (
         <div className={styles['container']}>
             <span className={styles['header']}><UserSVG className={styles['svg']} /> Login</span>
-            <form className={styles['form']}>
-                <input className={styles['input']} type="email" placeholder='Email' />
-                <input className={styles['input']} type="password" placeholder='Password' />
-                <input className={styles['btn']} type="submit" value='Login' />
+            <form className={styles['form']} onSubmit={handleSubmit}>
+                <input
+                    className={styles['input']}
+                    type="email"
+                    name='email'
+                    value={email}
+                    onChange={handleChange}
+                    placeholder='Email'
+                />
+                <input
+                    className={styles['input']}
+                    type="password"
+                    name='password'
+                    value={password}
+                    onChange={handleChange}
+                    placeholder='Password'
+                />
+                <input
+                    className={styles['btn']}
+                    type="submit"
+                    value='Login'
+                />
             </form>
             <Link to='/register' className={styles['redirect']}>
                 Not a member? Register Now!
--- client/src/components/Auth/RegisterComponent.js
+++ client/src/components/Auth/RegisterComponent.js
@@ -1,17 +1,66 @@
 import styles from './Auth.module.css'
 import { Link } from 'react-router-dom'
 import { ReactComponent as UserSVG } from '../../icons/user.svg'
+import { useState } from 'react'
 
 const RegisterComponent = () => {
+    const [user, setUser] = useState({
+        name: '', email: '', password1: '', password2: ''
+    })
+    const { name, email, password1, password2 } = user
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUser({ ...user, [name]: value })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password1 !== password2) {
+            console.log("Password don't match");
+            return
+        }
+        console.log(user);
+    }
     return (
         <div className={styles['container']}>
             <span className={styles['header']}><UserSVG className={styles['svg']} /> SignUp</span>
-            <form className={styles['form']}>
-                <input className={styles['input']} type="text" placeholder='Name' />
-                <input className={styles['input']} type="email" placeholder='Email' />
-                <input className={styles['input']} type="password" placeholder='Password' />
-                <input className={styles['input']} type="password" placeholder='Confirm Password' />
-                <input className={styles['btn']} type="submit" value='Register' />
+            <form className={styles['form']} onSubmit={handleSubmit}>
+                <input
+                    className={styles['input']}
+                    type="text"
+                    name='name'
+                    value={name}
+                    onChange={handleChange}
+                    placeholder='Name'
+                />
+                <input
+                    className={styles['input']}
+                    type="email"
+                    name='email'
+                    value={email}
+                    onChange={handleChange}
+                    placeholder='Email'
+                />
+                <input
+                    className={styles['input']}
+                    type="password"
+                    name='password1'
+                    value={password1}
+                    onChange={handleChange}
+                    placeholder='Password'
+                />
+                <input
+                    className={styles['input']}
+                    type="password"
+                    name='password2'
+                    value={password2}
+                    onChange={handleChange}
+                    placeholder='Confirm Password'
+                />
+                <input
+                    className={styles['btn']}
+                    type="submit"
+                    value='Register'
+                />
             </form>
             <Link to='/login' className={styles['redirect']}>
                 Already a member? Login
