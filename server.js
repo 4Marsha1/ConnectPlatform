649a6d0c0ab7eece4e808c0b403853628aaa6aad
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-03-22 19:51:21 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const colors = require('colors');
 const cors = require('cors');
 const config = require('config')
+const path = require('path')
 const connectDB = require('./config/db');
 const errorHandler = require('./middlewares/errorMiddleware')
 
@@ -15,15 +16,20 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-// TEST Route
-app.get('/', (req, res) => {
-    res.send('Test')
-})
-
 // ROUTES
 app.use('/api/users/', require('./routes/userRoutes'));
 app.use('/api/profile/', require('./routes/profileRoutes'));
 app.use('/api/posts/', require('./routes/postRoutes'));
+
+// FOR PRODUCTION
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // CUSTOM ERROR HANDLER
 app.use(errorHandler);
